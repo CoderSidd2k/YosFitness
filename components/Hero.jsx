c33f6350f5ff07242fda005599647487708cd459
CoderSidd2k <siddarthgaurav93@gmail.com>
2023-07-02 15:17:46 +0530

    v4

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="lg:ml-8 animate-slide-right">
         <h1 className="text-4xl font-bold text-white tracking-widest mt-4 italic animate-flicker-slow lg:text-5xl">Yos Fitness</h1>
 
-        <p className="hidden md:block text-lg text-left mt-3 italic text-red-500 tracking-wide">"Nothing is Fun Unless you do"</p>
+        <p className=" text-lg text-left mt-3 italic text-red-500 tracking-wide">"Nothing is Fun Unless you do"</p>
         <a href="https://forms.gle/2h9UW2D7gtan8EWo8" target="_blank" rel="noopener noreferrer">
          <button className="bg-red-600 text-white px-4 py-2 mt-3 rounded-none md:hidden">
             Contact us
