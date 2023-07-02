cbac76f2111015899acfe75cc8436042091abf2b
CoderSidd2k <siddarthgaurav93@gmail.com>
2023-07-02 16:08:03 +0530

    v7

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <div id="about" className="flex flex-col justify-center items-center lg:flex-row lg:justify-center lg:items-center h-screen pt-20">
       <div className="flex items-center">
-      <img className="w-64 h-64 animate-spin-slow" src={Logo.src} alt="" />
+      <img className="w-64 h-64 " src={Logo.src} alt="" />
       </div>
 
       <div className="w-full md:w-1/2 flex flex-col justify-center pl-8 text-white md:ml-8">
